cf34e904fc43cab897af176d7331278fee21b12c
Omar Derbala <derbalajr@gmail.com>
2024-07-30 05:10:41 +0300

    create hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Omar Derbala's Portfolio",
-  description: "Omar Derbala, full stack developer skilled in PHP, Laravel, and React. Explore projects and innovations.",
+  description:
+    "Omar Derbala, full stack developer skilled in PHP, Laravel, and React. Explore projects and innovations.",
 };
 
 export default function RootLayout({
@@ -16,7 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
