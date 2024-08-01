caf44d7a45ecca501104fa9a102b7cc647bb4bcb
Omar Derbala <derbalajr@gmail.com>
2024-08-01 04:47:35 +0300

    deployed with vercel and modify

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,20 +24,37 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89wv] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-85">
-          Blending Laravel&apos;s Elegance with Next.js&apos;s Efficiency
+            Blending Laravel&apos;s Elegance with Next.js&apos;s Efficiency
           </h2>
 
-          <TextGenerateEffect
+          {/* <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Developing Unique Ideas into State-of-the-Art Web Solutions"
-          />
+          /> */}
+          <div>
+            <div className="my-4">
+              <div className=" dark:text-white text-black">
+                <div className="text-center text-[40px] md:text-5xl lg:text-6xl">
+                  <h1 className="dark:text-white text-black font-bold leading-snug tracking-wide">
+                    Developing Unique Ideas into State-of-the-Art
+                    <span className="text-lime-400"> Web Solutions</span>
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
 
           <p className="text-center md:tracking-wider mb-4 text-xs md:text-lg lg:text-xl">
-            I&apos;m Omar Derbala, an Egyptian Full Stack Developer merging tech with Pharaohs&apos; majesty.
+            I&apos;m Omar Derbala, an Egyptian Full Stack Developer merging tech
+            with Pharaohs&apos; majesty.
           </p>
 
           <a href="#projects">
-            <MagicButton title="Show My Work" icon={<FaLocationArrow />} position="right" />
+            <MagicButton
+              title="Show My Work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
           </a>
         </div>
       </div>
