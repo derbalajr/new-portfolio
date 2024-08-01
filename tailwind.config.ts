afa224c38230d34442549ed6d37dd25caf926ddf
Omar Derbala <derbalajr@gmail.com>
2024-08-01 05:14:24 +0300

    deployed with vercel and modify

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -148,12 +148,17 @@ const config = {
         },
       },
       animation: {
-        // "accordion-down": "accordion-down 0.2s ease-out",
-        // "accordion-up": "accordion-up 0.2s ease-out",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
         spotlight: "spotlight  1 forwards",
-        // shimmer: "shimmer 2s linear infinite",
- 
-
+        shimmer: "shimmer 2s linear infinite",
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
     },
   },
